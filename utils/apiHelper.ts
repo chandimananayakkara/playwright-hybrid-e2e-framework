e180aee423164b605349dffae9715f88e2fff3b0
chandima <chandimananayakkara94@gmail.com>
2026-05-21 18:29:49 +0530

    feature/create test for signup

---
+++ utils/apiHelper.ts
@@ -0,0 +1,23 @@
+import { APIRequestContext, expect } from "@playwright/test";
+
+export class APIHelper {
+
+    static async createMessage(request: APIRequestContext, name:string, email:string, phone: number,  subject:string, description:string){
+
+        const response = await request.post('https://automationintesting.online/message/', {
+            data:{
+                name:name,
+                email: email,
+                phone: phone,
+                subject: subject,
+                description: description
+
+            }
+
+        })
+
+        expect(response.status()).toBe(201)
+
+
+    }
+}
